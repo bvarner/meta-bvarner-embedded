ebe652afd7784f375642102d36235a7a2666ba36
bvarner <bryan@varnernet.com>
2017-11-06 16:06:28 -0500

    Suitable for a time-lapse stream

--- recipes-multimedia/pistreamudp/files/pistreamudp.sh
+++ recipes-multimedia/pistreamudp/files/pistreamudp.sh
@@ -3,9 +3,10 @@ while : ; do
 	assignedip=`ip addr show | grep 'inet.*dynamic' | cut -d ' ' -f 6 -`
 	if [ -z "$assignedip" ]; then
 		echo "Waiting for IP..."
-		sleep 3
+		sleep 1
 	else
 		echo "Streaming to udp://224.0.0.1:8675"
-		raspivid -n -o - -t 0 -fps 25 -g 125 -ih -pf high -lev 4.2 -if both -b 768000 | ffmpeg -re -an -i - -vcodec copy -f mpegts udp://224.0.0.1:8675 -loglevel quiet
+		# These settings produce video quality congruent with with an iPhone 5s on time-lapse.
+		raspivid -w 1280 -h 720 -n -o - -t 0 -fps 30 -g 45 -ih -pf main -lev 4 -if both -b 12475000 | ffmpeg -re -an -i - -vcodec copy -f mpegts udp://224.0.0.1:8675 -loglevel quiet
 	fi
 done
